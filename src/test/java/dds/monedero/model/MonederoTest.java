f94fbc3f1626092664fa1025c1eed6f22352cff6
Matias Beorlegui <mbeorlegui@frba.utn.edu.ar>
2022-05-14 17:09:11 -0300

    Fix tests

--- src/test/java/dds/monedero/model/MonederoTest.java
+++ src/test/java/dds/monedero/model/MonederoTest.java
@@ -5,8 +5,10 @@ import dds.monedero.exceptions.MaximoExtraccionDiarioException;
 import dds.monedero.exceptions.MontoNegativoException;
 import dds.monedero.exceptions.SaldoMenorException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MonederoTest {
@@ -17,24 +19,31 @@ public class MonederoTest {
     cuenta = new Cuenta();
   }
 
+
   @Test
+  @DisplayName("Al poner 1500 pesos en una cuenta nueva, el saldo de esa cuenta es 1500")
   void Poner() {
     cuenta.poner(1500);
+    assertEquals(cuenta.getSaldo(), 1500);
   }
 
   @Test
+  @DisplayName("No puede ponerse un monto negativo en la cuenta")
   void PonerMontoNegativo() {
     assertThrows(MontoNegativoException.class, () -> cuenta.poner(-1500));
   }
 
   @Test
+  @DisplayName("Al hacerse 3 depositos en una cuenta nueva, los mismos son tomados adecuadamente")
   void TresDepositos() {
     cuenta.poner(1500);
     cuenta.poner(456);
     cuenta.poner(1900);
+    assertEquals(cuenta.getSaldo(), 3856);
   }
 
   @Test
+  @DisplayName("No pueden hacerse mas de 3 depositos en el mismo dia en la cuenta")
   void MasDeTresDepositos() {
     assertThrows(MaximaCantidadDepositosException.class, () -> {
           cuenta.poner(1500);
@@ -44,7 +53,9 @@ public class MonederoTest {
     });
   }
 
+
   @Test
+  @DisplayName("Al extraer mas que el saldo de la cuenta, arroja una excepcion")
   void ExtraerMasQueElSaldo() {
     assertThrows(SaldoMenorException.class, () -> {
           cuenta.setSaldo(90);
@@ -53,6 +64,7 @@ public class MonederoTest {
   }
 
   @Test
+  @DisplayName("Al extraer 1001 de la cuenta, arroja una excepcion")
   public void ExtraerMasDe1000() {
     assertThrows(MaximoExtraccionDiarioException.class, () -> {
       cuenta.setSaldo(5000);
@@ -61,6 +73,7 @@ public class MonederoTest {
   }
 
   @Test
+  @DisplayName("Al extraer un monto negativo de la cuenta, arroja una excepcion")
   public void ExtraerMontoNegativo() {
     assertThrows(MontoNegativoException.class, () -> cuenta.sacar(-500));
   }
