68a0c89e764fff18fe2fb941330c4569b15bf5f5
Matias Beorlegui <mbeorlegui@frba.utn.edu.ar>
2022-05-15 17:09:03 -0300

    Utilizo abstraccion fueExtraido

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -63,7 +63,7 @@ public class Cuenta {
 
   public double getMontoExtraidoA(LocalDate fecha) {
     return getMovimientos().stream()
-        .filter(movimiento -> !movimiento.isDeposito() && movimiento.getFecha().equals(fecha)) // Aca conviene utilizar la abstraccion fueExtraido(fecha)
+        .filter(movimiento -> movimiento.fueExtraido(fecha))
         .mapToDouble(Movimiento::getMonto)
         .sum();
   }
